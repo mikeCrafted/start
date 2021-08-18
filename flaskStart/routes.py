1029675b7ed2052add5b67d29ac250e75515be5b
mikeCrafted <michael.pisnyachevskiy@gmx.at>
2021-08-18 19:11:20 +0200

    adding requirements

--- flaskStart/routes.py
+++ flaskStart/routes.py
@@ -11,15 +11,24 @@ def index():
 def create():
     req = request.get_json()
     print(req)
-    temp_dir_name = secrets.token_hex(16)
-    os.mkdir(f'{app.root_path}\\generated\\{temp_dir_name}')
+    temp_dir = f"{app.root_path}\\generated\\{secrets.token_hex(16)}"
+    project_dir = f"{temp_dir}\\{req['projectName']}"
+    os.mkdir(temp_dir)
+    os.mkdir(project_dir)
+    os.mkdir(f"{project_dir}\\{req['projectName']}")
+    # creating virtual environment
     if req['virtEnv']['show'] == True:
         virt_env_data = req['virtEnv']
         create_virt_env(name = virt_env_data['name'],
                         use_system_site_packages = virt_env_data['params'][0],
                         use_clear = virt_env_data['params'][1], 
                         use_with_pip = virt_env_data['params'][2],
-                        target_dir = temp_dir_name)
+                        target_dir = project_dir)
+    # creating requirements file
+    with open(f'{project_dir}\\requirements.txt', 'w') as f:
+        for package in req['requirements']:
+            f.write(f"{package['name']}{package['type']}{package['version']}\n")
+    
     return make_response(jsonify('ok'), 200)
 
 # DB
@@ -31,4 +40,4 @@ def create():
 def create_virt_env(name, use_system_site_packages, use_clear, use_with_pip, target_dir):
     my_env_builder = EnvBuilder(system_site_packages = use_system_site_packages,
                                 clear = use_clear, with_pip = use_with_pip)
-    my_env_builder.create(f'{app.root_path}\\generated\\{target_dir}\\{name}')
+    my_env_builder.create(f'{target_dir}\\{name}')
