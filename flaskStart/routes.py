86bbf9b8490262b517de3661f9ad9000f8cc0adf
mikeCrafted <michael.pisnyachevskiy@gmx.at>
2021-09-21 12:36:41 +0200

    extracting helper functions in seperate file

--- flaskStart/routes.py
+++ flaskStart/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, make_response, jsonify
 from flaskStart import app, SOURCES
 from venv import EnvBuilder
 import secrets, os, re
-
+from helper_functions import *
 
 @app.route('/')
 def index():        
@@ -160,27 +160,6 @@ def create_routes_file(req, project_dir):
     with open(f'{project_dir}\\routes.py', 'w') as f:
         f.write(data)
 
-# helper function to return all imports needed for __init__ or routes file based on user selection
-def get_imports(req, destination):
-    imports = ""
-    if req['addDatabase']:
-        if destination == 'init':
-            imports += "from flask_sqlalchemy import SQLAlchemy\n"
-        else:
-            imports += ', db'
-    if req['addAuthSys']:
-        if destination == 'init':
-            imports += "from flask_bcrypt import Bcrypt\n"
-            imports += "from flask_login import LoginManager\n"
-        else:
-            imports += ', bcrypt'
-    if req['emails']['show']:
-        if destination == 'init':
-            imports += "from flask_mail import Mail"
-        else:
-            imports += ', mail'
-    return imports
-
 def create_models_file(req, project_dir):
     if req['addAuthSys']:
         data = generate_model_string(req)
@@ -218,97 +197,8 @@ def create_frontend(frontend, project_dir, project_name):
     
     # creating additional css file for checkbuttons and radiobuttons styles
     if frontend['checkRad']:
-        create_checkradio_css_file()
+        create_checkradio_css_file(project_dir, SOURCES)
 
     # creating js file
     if frontend['addJs']:
         create_js_file(frontend['addNavBar'], project_dir, navbar)
-    
-    
-
-def create_css_link(filename):
-    return f"<link rel=\"stylesheet\" type=\"text/css\" href=\"{{{{ url_for('static', filename = '{filename}') }}}}\">"
-
-def generate_db_config_string(db_type):
-    if db_type == 'MySQL':
-        db_config = "app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/site'\n"
-    else:
-        db_config = "app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'\n"
-    db_config += "db = SQLAlchemy(app)\n"
-    db_config += "db.create_all()\n"
-    db_config += "db.session.commit()\n\n"
-    return db_config
-
-def generate_login_config_string(login_view):
-    login_config = "bcrypt = Bcrypt(app)\n"
-    # setting up loginManager
-    login_config += "login_manager = LoginManager(app)\n"
-    login_config += f"login_manager.login_view = '{login_view}'\n"
-    login_config += "login_manager.login_message_category = 'info'\n\n"
-    return login_config
-
-def generate_email_config_string():
-    email_config  = "app.config['MAIL_SERVER'] = ''          # for example 'smtp.gmail.com'\n"
-    email_config += "app.config['MAIL_PORT'] = ''            # for example 587\n"
-    email_config += "app.config['MAIL_USE_TLS'] = True\n"
-    email_config += "app.config['MAIL_USERNAME'] = ''        # your mail username\n"
-    email_config += "app.config['MAIL_PASSWORD'] = ''        # Don't set your password here as string, use environment variables\n"
-    email_config += "mail = Mail(app)\n\n"
-    return email_config
-
-def generate_blueprint_imports_string(project_name, blueprint_name):
-    data = f"from {project_name} import app\n" 
-    data += "from flask import Blueprint, render_template, url_for, redirect, request\n\n"
-    data += f"{blueprint_name} = Blueprint('{blueprint_name}', __name__)\n"
-    data += f"# Use @{blueprint_name}.route()"
-    return data
-
-def replace_placeholder(data, condition, placeholder, new_value, placeholder_if_false = ""):
-    if condition:
-        data = data.replace(placeholder, new_value)
-    else:
-        if placeholder_if_false == "":
-            data = data.replace(f"{placeholder}\n", '')
-        else:
-            data = data.replace(placeholder_if_false, '')
-    return data
-
-def generate_model_string(req):
-    data = f"from {req['projectName']} import db, login_manager\n"
-    data += "from flask_login import UserMixin\n\n"
-    data += "@login_manager.user_loader\n"
-    data += "def load_user(user_id):\n\t"
-    data += f"return {req['authSys']['userTableName']}.query.get(int(user_id))\n\n"
-    data += f"class {req['authSys']['userTableName']}(db.Model, UserMixin):\n"
-    for field in req['authSys']['userTableFields']:
-        data += f"\t{field['name']} = db.Column(db.{field['type']}, primary_key = {field['pk']}, nullable = {field['nullable']}, unique = {field['unique']})\n"
-    return data
-
-def get_navbar_substring(navbar, section):
-    return navbar[navbar.index(section) + len(section) : navbar.index(section.replace('start', 'end'))]
-
-def handle_index_and_layout_creation(frontend, project_dir, data):
-    if frontend['layout'] and frontend['index']:
-        with open(f"{project_dir}\\templates\\layout.html", 'w') as f:
-            f.write(data)
-        with open(f"{project_dir}\\templates\\index.html", 'w') as f:
-            f.write("{% extends \"layout.html\" %}\n\n{% block content %}\n\n{% endblock content %}")
-    elif frontend['layout']:
-        with open(f"{project_dir}\\templates\\layout.html", 'w') as f:
-            f.write(data)
-    elif frontend['index']:
-        with open(f"{project_dir}\\templates\\index.html", 'w') as f:
-            f.write(data)
-
-def create_js_file(add_navbar, project_dir, navbar):
-    js = ''
-    if add_navbar:
-        js = get_navbar_substring(navbar, '[[ navbar_js_start ]]')
-    with open(f"{project_dir}\\static\\main.js", 'w') as f:
-        f.write(js)
-
-def create_checkradio_css_file():
-    with open(f'{SOURCES}\\checkRad.txt', 'r') as f:
-        check_rad_css = f.read()
-    with open(f"{project_dir}\\static\\checkradio.css", 'w') as f:
-        f.write(check_rad_css)
